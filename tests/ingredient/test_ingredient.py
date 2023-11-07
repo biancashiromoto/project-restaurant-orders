43d83a4dc401b2174983b726f85173496dd17aa7
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-11-07 15:25:12 -0300

    test: Add test cases for class Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,27 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    # pass
+    camarao = Ingredient("camarão")
+    assert camarao.name == "camarão"
+
+    restrictions = {
+        Restriction.ANIMAL_MEAT,
+        Restriction.SEAFOOD,
+        Restriction.ANIMAL_DERIVED
+    }
+    assert camarao.restrictions == restrictions
+
+    assert camarao.__hash__() == hash(camarao.name)
+
+    camarao_2 = Ingredient("camarão")
+    assert camarao.__eq__(camarao_2) is True
+
+    berinjela = Ingredient("berinjela")
+    assert camarao.__eq__(berinjela) is False
+
+    assert camarao.__repr__() == "Ingredient('camarão')"
+
+
+test_ingredient()
