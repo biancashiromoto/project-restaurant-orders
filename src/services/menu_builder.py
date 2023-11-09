45d7ed687d05c581bac13c54b6e1faddcf5eae4d
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-11-09 20:11:08 -0300

    feat: Implement method get_main_menu

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -26,4 +26,31 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> List[Dict]:
-        pass
+        main_menu = []
+
+        for dish in self.menu_data.dishes:
+            """caso haja alguma restrição e a restrição esteja
+            presente em alguma das receitas"""
+            if restriction and restriction in dish.get_restrictions():
+                continue
+            """para cada ingrediente presente na receita,
+            caso o ingrediente exista no inventário e sua quantidade
+            seja maior que 0,"""
+            if all(
+                ingredient in self.inventory.inventory
+                and self.inventory.inventory[ingredient] > 0
+                for ingredient in dish.recipe.keys()
+            ):
+                # adiciona a receita ao array
+                main_menu.append({
+                    "dish_name": dish.name,
+                    "restrictions": dish.get_restrictions(),
+                    "price": dish.price,
+                    "ingredients": dish.get_ingredients(),
+                })
+
+        return main_menu
+
+
+teste = MenuBuilder()
+print(MenuBuilder.get_main_menu(teste))
